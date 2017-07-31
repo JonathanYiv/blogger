41f6e9e4f791972d12f40dd5bb7cfef381a48346
Jonathan Yiv <JonathanYiv@gmail.com>
2017-07-31 16:20:47 -0400

    Finished I2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
 	root to: 'articles#index'
   
-	resources :articles
+	resources :articles do
+		resources :comments
+	end
 	# This line tells Rails that we have a resource named articles and the router should expect requests to fiollow the RESTful model of web interaction
 	# the router will implicitly understand things like /articles and /articles/new
 
