45278e056db1dad159af668afc665584b5bf01cf
Jonathan Yiv <JonathanYiv@gmail.com>
2017-07-31 17:01:31 -0400

    Added Tagging from I3

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,7 @@ Rails.application.routes.draw do
 	resources :articles do
 		resources :comments
 	end
-	# This line tells Rails that we have a resource named articles and the router should expect requests to fiollow the RESTful model of web interaction
-	# the router will implicitly understand things like /articles and /articles/new
-
-
+	resources :tags
 
 
 
