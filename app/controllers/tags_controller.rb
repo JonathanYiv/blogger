495432364a41b9954506cfc068f1779457c8a66b
Jonathan Yiv <JonathanYiv@gmail.com>
2017-07-31 23:45:08 -0400

    Added comment deleting, tag deleting, and necessary relationships.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,6 +4,12 @@ class TagsController < ApplicationController
 		@tag = Tag.all
 	end
 
+	def destroy
+		@tag = Tag.find(params[:id])
+		@tag.destroy
+		redirect_back(fallback_location: root_path)
+	end
+
 	def show
 		@tag = Tag.find(params[:id])
 	end
